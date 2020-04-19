7220da41e56284c75f425eb9792efa138a9a9a5c
Aleks <aleks_bork@yahoo.com>
2020-04-18 19:51:36 -0500

    iris experiment
    additional files

--- create_jobs.sh
+++ create_jobs.sh
@@ -1,5 +1,5 @@
 # create job.yaml files
 for i in $(eval echo {1..$1})
 do
-  cat cifar10-job-template.yml | sed "s/\$ITEM/$i/" > ./hyperparam-jobs-specs/cifar10-job-$i.yml
+  cat iris-job-template.yml | sed "s/\$ITEM/$i/" > ./hyperparam-jobs-specs/iris-job-$i.yml
 done
--- generate_hyperparam_combinations.py
+++ generate_hyperparam_combinations.py
@@ -3,11 +3,13 @@ import yaml
 
 
 hyper_params = {
-    'max_learning_rate': [0.4, 0.6],
-    'data_aug_cutout_size': [5, 12],
-    'batch_size': [128, 512],
-    'momentum': [0.9, 0.99],
-    'batch_norm': ['on']
+    #'max_learning_rate': [0.4, 0.6],
+    #'data_aug_cutout_size': [5, 12],
+    #'batch_size': [128, 512],
+    #'momentum': [0.9, 0.99],
+    #'batch_norm': ['on']
+    'max_depth': [5, 8, 10],
+    'n_estim': [100, 200, 300]
 }
 
 keys, values = zip(*hyper_params.items())
@@ -15,9 +17,9 @@ hyperparam_allsets = [dict(hyperparam_set=dict(zip(keys, v))) for v in itertools
 
 print("Total number of hyperparameter sets: "+str(len(hyperparam_allsets)))
 
-with open('hyperparams.yml', 'w') as outfile:
+with open('rf_hyperparams.yml', 'w') as outfile:
     yaml.dump(hyperparam_allsets, outfile, default_flow_style=False)
 
-print("Hyperparameter sets saved to: "+'hyperparams.yml')
+print("Hyperparameter sets saved to: "+'rf_hyperparams.yml')
 
 
